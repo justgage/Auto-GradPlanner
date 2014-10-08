fdb58f3f2170f554240e9d87d9107aa399da6816
Gage Peterson <justgage@gmail.com>
2014-10-08 01:31:40 -0600

    basic information

---
+++ gradPlan.pl
@@ -0,0 +1,638 @@
+% ... 
+% AUTO GRAD PLANNER
+% by: Gage Peterson (gagekpeterson@gmail.com)
+% Info-------------------------
+% NOTE this only is going to be 100% acurate for 2012 calender year
+
+
+% how to plan: 
+% classes that have no prerequisites go first
+% classes that have prerequisites to the classes that don't go next (or latter)
+% classes that have prerequisites to the previous go in the 3rd list 
+% no TIME should add up to more than 18 credits (or whatever)
+% only one religion class per semester
+% all classes must be in the plan
+% no duplicates
+
+
+% Course scheduling
+%cs124 
+offered(cs124, w2014, y).
+offered(cs124, s2014, y).
+offered(cs124, f2014, y).
+
+offered(cs124, w2015, y).
+offered(cs124, s2015, y).
+offered(cs124, f2015, y).
+
+offered(cs124, w2016, y).
+offered(cs124, s2016, y).
+offered(cs124, f2016, y).
+
+offered(cs124, w2017, y).
+offered(cs124, s2017, y).
+offered(cs124, f2017, y).
+
+%cs165 
+offered(cs165, w2014, y).
+offered(cs165, s2014, y).
+offered(cs165, f2014, y).
+
+offered(cs165, w2015, y).
+offered(cs165, s2015, y).
+offered(cs165, f2015, y).
+
+offered(cs165, w2016, y).
+offered(cs165, s2016, y).
+offered(cs165, f2016, y).
+
+offered(cs165, w2017, y).
+offered(cs165, s2017, y).
+offered(cs165, f2017, y).
+
+%cs213 
+offered(cs213, w2014, y).
+offered(cs213, s2014, n).
+offered(cs213, f2014, y).
+
+offered(cs213, w2015, y).
+offered(cs213, s2015, n).
+offered(cs213, f2015, y).
+
+offered(cs213, w2016, y).
+offered(cs213, s2016, n).
+offered(cs213, f2016, y).
+
+offered(cs213, w2017, y).
+offered(cs213, s2017, n).
+offered(cs213, f2017, y).
+
+%cs235
+offered(cs235, w2014, y).
+offered(cs235, s2014, y).
+offered(cs235, f2014, y).
+
+offered(cs235, w2015, y).
+offered(cs235, s2015, y).
+offered(cs235, f2015, y).
+
+offered(cs235, w2016, y).
+offered(cs235, s2016, y).
+offered(cs235, f2016, y).
+
+offered(cs235, w2017, y).
+offered(cs235, s2017, y).
+offered(cs235, f2017, y).
+
+%cs237
+offered(cs237, w2014, y).
+offered(cs237, s2014, y).
+offered(cs237, f2014, y).
+
+offered(cs237, w2015, y).
+offered(cs237, s2015, y).
+offered(cs237, f2015, y).
+
+offered(cs237, w2016, y).
+offered(cs237, s2016, y).
+offered(cs237, f2016, y).
+
+offered(cs237, w2017, y).
+offered(cs237, s2017, y).
+offered(cs237, f2017, y).
+
+%cs238
+offered(cs238, w2014, y).
+offered(cs238, s2014, n).
+offered(cs238, f2014, y).
+
+offered(cs238, w2015, y).
+offered(cs238, s2015, n).
+offered(cs238, f2015, y).
+
+offered(cs238, w2016, y).
+offered(cs238, s2016, n).
+offered(cs238, f2016, y).
+
+offered(cs238, w2017, y).
+offered(cs238, s2017, n).
+offered(cs238, f2017, y).
+
+%cs241
+offered(cs241, w2014, n).
+offered(cs241, s2014, n).
+offered(cs241, f2014, y).
+
+offered(cs241, w2015, y).
+offered(cs241, s2015, y).
+offered(cs241, f2015, y).
+
+offered(cs241, w2016, y).
+offered(cs241, s2016, y).
+offered(cs241, f2016, y).
+
+offered(cs241, w2017, y).
+offered(cs241, s2017, y).
+offered(cs241, f2017, y).
+
+%cs246
+offered(cs246, w2014, y).
+offered(cs246, s2014, y).
+offered(cs246, f2014, y).
+
+offered(cs246, w2015, y).
+offered(cs246, s2015, y).
+offered(cs246, f2015, y).
+
+offered(cs246, w2016, y).
+offered(cs246, s2016, y).
+offered(cs246, f2016, y).
+
+offered(cs246, w2017, y).
+offered(cs246, s2017, y).
+offered(cs246, f2017, y).
+
+%cs306
+offered(cs306, w2014, y).
+offered(cs306, s2014, n).
+offered(cs306, f2014, n).
+
+offered(cs306, w2015, y).
+offered(cs306, s2015, n).
+offered(cs306, f2015, n).
+
+offered(cs306, w2016, y).
+offered(cs306, s2016, n).
+offered(cs306, f2016, n).
+
+offered(cs306, w2017, y).
+offered(cs306, s2017, n).
+offered(cs306, f2017, n).
+
+%cs308
+offered(cs308, w2014, y).
+offered(cs308, s2014, y).
+offered(cs308, f2014, y).
+
+offered(cs308, w2015, y).
+offered(cs308, s2015, y).
+offered(cs308, f2015, y).
+
+offered(cs308, w2016, y).
+offered(cs308, s2016, y).
+offered(cs308, f2016, y).
+
+offered(cs308, w2017, y).
+offered(cs308, s2017, y).
+offered(cs308, f2017, y).
+
+%cs312
+offered(cs312, w2014, n).
+offered(cs312, s2014, y).
+offered(cs312, f2014, n).
+
+offered(cs312, w2015, n).
+offered(cs312, s2015, y).
+offered(cs312, f2015, n).
+
+offered(cs312, w2016, y).
+offered(cs312, s2016, n).
+offered(cs312, f2016, n).
+
+offered(cs312, w2017, y).
+offered(cs312, s2017, n).
+offered(cs312, f2017, n).
+
+%cs313
+offered(cs313, w2014, y).
+offered(cs313, s2014, y).
+offered(cs313, f2014, y).
+
+offered(cs313, w2015, y).
+offered(cs313, s2015, y).
+offered(cs313, f2015, y).
+
+offered(cs313, w2016, y).
+offered(cs313, s2016, y).
+offered(cs313, f2016, y).
+
+offered(cs313, w2017, y).
+offered(cs313, s2017, y).
+offered(cs313, f2017, y).
+
+%cs345
+offered(cs345, w2014, y).
+offered(cs345, s2014, y).
+offered(cs345, f2014, n).
+
+offered(cs345, w2015, n).
+offered(cs345, s2015, y).
+offered(cs345, f2015, n).
+
+offered(cs345, w2016, n).
+offered(cs345, s2016, y).
+offered(cs345, f2016, n).
+
+offered(cs345, w2017, n).
+offered(cs345, s2017, y).
+offered(cs345, f2017, n).
+
+%cs364
+offered(cs364, w2014, y).
+offered(cs364, s2014, y).
+offered(cs364, f2014, y).
+
+offered(cs364, w2015, y).
+offered(cs364, s2015, y).
+offered(cs364, f2015, y).
+
+offered(cs364, w2016, y).
+offered(cs364, s2016, y).
+offered(cs364, f2016, y).
+
+offered(cs364, w2017, y).
+offered(cs364, s2017, y).
+offered(cs364, f2017, y).
+
+%cs371
+offered(cs371, w2014, y).
+offered(cs371, s2014, y).
+offered(cs371, f2014, y).
+
+offered(cs371, w2015, y).
+offered(cs371, s2015, y).
+offered(cs371, f2015, y).
+
+offered(cs371, w2016, y).
+offered(cs371, s2016, y).
+offered(cs371, f2016, y).
+
+offered(cs371, w2017, y).
+offered(cs371, s2017, y).
+offered(cs371, f2017, y).
+
+%cs416
+offered(cs416, w2014, n).
+offered(cs416, s2014, y).
+offered(cs416, f2014, n).
+
+offered(cs416, w2015, n).
+offered(cs416, s2015, y).
+offered(cs416, f2015, n).
+
+offered(cs416, w2016, n).
+offered(cs416, s2016, y).
+offered(cs416, f2016, n).
+
+offered(cs416, w2017, n).
+offered(cs416, s2017, y).
+offered(cs416, f2017, n).
+
+%cs432
+offered(cs432, w2014, y).
+offered(cs432, s2014, y).
+offered(cs432, f2014, n).
+
+offered(cs432, w2015, n).
+offered(cs432, s2015, y).
+offered(cs432, f2015, n).
+
+offered(cs432, w2016, n).
+offered(cs432, s2016, y).
+offered(cs432, f2016, n).
+
+offered(cs432, w2017, n).
+offered(cs432, s2017, y).
+offered(cs432, f2017, n).
+
+%cs450
+offered(cs450, w2014, n).
+offered(cs450, s2014, n).
+offered(cs450, f2014, n).
+
+offered(cs450, w2015, n).
+offered(cs450, s2015, y).
+offered(cs450, f2015, n).
+
+offered(cs450, w2016, n).
+offered(cs450, s2016, y).
+offered(cs450, f2016, n).
+
+offered(cs450, w2017, n).
+offered(cs450, s2017, y).
+offered(cs450, f2017, n).
+
+%cs460
+offered(cs460, w2014, y).
+offered(cs460, s2014, n).
+offered(cs460, f2014, n).
+
+offered(cs460, w2015, y).
+offered(cs460, s2015, n).
+offered(cs460, f2015, n).
+
+offered(cs460, w2016, y).
+offered(cs460, s2016, n).
+offered(cs460, f2016, n).
+
+offered(cs460, w2017, y).
+offered(cs460, s2017, n).
+offered(cs460, f2017, n).
+
+%cs470 
+offered(cs470, w2014, y).
+offered(cs470, s2014, n).
+offered(cs470, f2014, n).
+
+offered(cs470, w2015, y).
+offered(cs470, s2015, n).
+offered(cs470, f2015, n).
+
+offered(cs470, w2016, y).
+offered(cs470, s2016, n).
+offered(cs470, f2016, n).
+
+offered(cs470, w2017, y).
+offered(cs470, s2017, n).
+offered(cs470, f2017, n).
+
+%cs480 
+offered(cs480, w2014, n).
+offered(cs480, s2014, y).
+offered(cs480, f2014, n).
+
+offered(cs480, w2015, n).
+offered(cs480, s2015, y).
+offered(cs480, f2015, n).
+
+offered(cs480, w2016, n).
+offered(cs480, s2016, y).
+offered(cs480, f2016, n).
+
+offered(cs480, w2017, n).
+offered(cs480, s2017, y).
+offered(cs480, f2017, n).
+
+%cs490 
+offered(cs490, w2014, n).
+offered(cs490, s2014, y).
+offered(cs490, f2014, n).
+
+offered(cs490, w2015, n).
+offered(cs490, s2015, y).
+offered(cs490, f2015, n).
+
+offered(cs490, w2016, n).
+offered(cs490, s2016, y).
+offered(cs490, f2016, n).
+
+offered(cs490, w2017, n).
+offered(cs490, s2017, y).
+offered(cs490, f2017, n).
+
+%cs499 
+offered(cs499, w2014, y).
+offered(cs499, s2014, y).
+offered(cs499, f2014, y).
+
+offered(cs499, w2015, y).
+offered(cs499, s2015, y).
+offered(cs499, f2015, y).
+
+offered(cs499, w2016, y).
+offered(cs499, s2016, y).
+offered(cs499, f2016, y).
+
+offered(cs499, w2017, y).
+offered(cs499, s2017, y).
+offered(cs499, f2017, y).
+
+%ecen150 
+offered(ecen150, w2014, y).
+offered(ecen150, s2014, y).
+offered(ecen150, f2014, y).
+
+offered(ecen150, w2015, y).
+offered(ecen150, s2015, y).
+offered(ecen150, f2015, y).
+
+offered(ecen150, w2016, y).
+offered(ecen150, s2016, y).
+offered(ecen150, f2016, y).
+
+offered(ecen150, w2017, y).
+offered(ecen150, s2017, y).
+offered(ecen150, f2017, y).
+
+%ecen160 
+offered(ecen160, w2014, y).
+offered(ecen160, s2014, y).
+offered(ecen160, f2014, y).
+
+offered(ecen160, w2015, y).
+offered(ecen160, s2015, y).
+offered(ecen160, f2015, y).
+
+offered(ecen160, w2016, y).
+offered(ecen160, s2016, y).
+offered(ecen160, f2016, y).
+
+offered(ecen160, w2017, y).
+offered(ecen160, s2017, y).
+offered(ecen160, f2017, y).
+
+%ecen250 
+offered(cs250, w2014, y).
+offered(cs250, s2014, y).
+offered(cs250, f2014, n).
+
+offered(cs250, w2015, y).
+offered(cs250, s2015, y).
+offered(cs250, f2015, n).
+
+offered(cs250, w2016, y).
+offered(cs250, s2016, y).
+offered(cs250, f2016, n).
+
+offered(cs250, w2017, y).
+offered(cs250, s2017, y).
+offered(cs250, f2017, n).
+
+
+%ecen260 
+
+%ecen324 
+
+%ecen340 
+
+%ecen350 
+
+%ecen351
+
+%(ecen 450)
+
+%ecen361
+
+%(ecen 460)
+
+%ecen380 
+
+%ecen390 
+
+%ecen410 
+
+%ecen420 
+
+%ecen430 
+
+%ecen440 
+
+%ecen451 
+
+%ecen461 
+
+%ecen470 
+
+%ecen480 
+
+%ecen490 
+
+%ecen499 
+
+
+% online only
+onlineOnly(cs313, f2014).
+onlineOnly(cs313, f2015).
+onlineOnly(cs313, f2016).
+onlineOnly(cs313, f2017).
+onlineOnly(cs364, f2014).
+onlineOnly(cs364, f2015).
+onlineOnly(cs364, f2016).
+onlineOnly(cs364, f2017).
+onlineOnly(cs371, f2014).
+onlineOnly(cs371, f2015).
+onlineOnly(cs371, f2016).
+onlineOnly(cs371, f2017).
+onlineOnly(cs416, f2014).
+onlineOnly(cs416, f2015).
+onlineOnly(cs416, f2016).
+onlineOnly(cs416, f2017).
+
+requires(cit320,  cs124).
+requires(cs165,   cs124).
+requires(cs213,   cs165).
+requires(cs235,   cs165).
+requires(cs237,   cs165).
+requires(cs246,   cs235).
+requires(cs312,   cs235).
+requires(cs312,   cs235).
+requires(ecen160, cs124).
+requires(ecen324, cs235).
+
+credits(bio240, 4).
+credits(chem105, 4).
+credits(chem106, 4).
+credits(cit320, 3).
+credits(cs124, 3).
+credits(cs165, 3).
+credits(cs213, 3).
+credits(cs235, 3).
+credits(cs237, 3).
+credits(cs238, 3).
+credits(cs246, 3).
+credits(cs306, 3).
+credits(cs308, 2).
+credits(cs312, 3).
+credits(cs313, 3).
+credits(cs345, 3).
+credits(cs364, 4).
+credits(cs371, 3).
+credits(cs398, 1-4).
+credits(cs416, 2).
+credits(cs432, 3).
+credits(cs460, 3).
+credits(cs470, 3).
+credits(cs480, 3).
+credits(cs490R, 3).
+credits(cs498R, 1-4).
+credits(cs499, 3).
+credits(cs499A, 2).
+credits(cs499B, 1).
+credits(ecen160, 3).
+credits(ecen324, 3).
+credits(ecen460, 3).
+credits(math113, 3).
+credits(math325, 3).
+credits(math341, 3).
+credits(math411, 3).
+credits(ph121, 3).
+credits(ph150, 1).
+credits(ph220, 3).
+
+className(cs124,'Introduction to Software Development').
+className(cs165, 'Object-oriented Software Development').
+className(cs213, 'Web Engineering 1').
+className(cs235, 'Data Structures').
+className(cs237, 'Discrete Mathematics 1').
+className(cs238, 'Discrete Mathematics 2').
+className(cs246, 'Software Design and Development').
+className(cs290, 'Special Topics').
+className(cs306, 'Algorithms and Complexity ').
+className(cs308, 'Technical Communication ').
+className(cs312, 'Computer Graphics ').
+className(cs313, 'Web Engineering 2 ').
+className(cs345, 'Operating Systems ').
+className(cs364, 'Software Engineering 1 ').
+className(cs371, 'Human-Computer Interaction').
+className(cs398, 'Internship ').
+className(cs416, 'Software Engineering 2 ').
+className(cs432, 'Software Engineering 3').
+className(cs460, 'Computer Communication and Networks').
+className(cs470, 'Computer Security').
+className(cs480, 'Computational Theory').
+className(cs490R, 'Special Topics').
+className(cs498R, 'Internship').
+className(cs499, 'Senior Project').
+className(cs499A, 'Senior Project, Part A').
+className(cs499B, 'Senior Project, Part B').
+className(cs499S, 'Senior Project Supplemental').
+className(ecen150, 'Electric Circuit Analysis 1 ').
+className(ecen160, 'Fundamentals of Digital Systems').
+className(ecen250, 'Electric Circuit Analysis 2').
+className(ecen260, 'Microprocessor-Based System Design').
+className(ecen324, 'Computer Architecture').
+className(ecen340, 'Digital Systems Design').
+className(ecen350, 'Electronic Devices and Circuits').
+className(ecen380, 'Signals and Systems').
+className(ecen390, 'Electricity and Magnetism').
+className(ecen398R, 'Internship').
+className(ecen420, 'RF Circuits').
+className(ecen430, 'Power Electronics').
+className(ecen440, 'Data and Computer Communications').
+className(ecen450, 'Digital VLSI System Design').
+className(ecen460, 'Real-Time and Embedded Systems').
+className(ecen470, 'Feedback Control of Dynamic Systems').
+className(ecen480, 'Digital Signal Processing').
+className(ecen490, 'Special Topics').
+
+
+requiresAll(Class, X) :- requires(Class, X).
+requiresAll(Class, X) :- requires(Class, Z), requiresAll(Z, X).
+
+afterRequirements((Class, Time), [(Prerec, PreTime)]) :-
+   integer(Time),
+   integer(PreTime),
+   requiresAll(Class, Prerec),
+   Time > PreTime.
+
+afterRequirements((Class, Time), [(Prerec, PreTime)| Other]) :-
+   integer(Time),
+   integer(PreTime),
+   requiresAll(Class, Prerec),
+   Time > PreTime,
+   afterRequirements((Class, Time), Other).
+
+
+%afterRequirements((Class, Time), [(Class, Time)]).
+%afterRequirements((Class, Time), [(Class, Time)| Other]) :-
+%afterRequirements((Class, Time), Other).
